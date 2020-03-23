db5576aaad858af64808c15812fc8145501e8ebd
Mugeesh Husain <mugeesh@gmail.com>
2020-03-23 22:46:09 +0800

    initial comment javabrain

---
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,33 @@
+package io.javabrains.movieinfoservice.resources;
+
+import io.javabrains.movieinfoservice.models.Movie;
+import io.javabrains.movieinfoservice.models.MovieSummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+    	String URLs="https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" +  apiKey;
+    	System.out.println(URLs);
+        MovieSummary movieSummary = restTemplate.getForObject(URLs, MovieSummary.class);
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+
+    }
+    //597
+    //https://api.themoviedb.org/3/movie/550?api_key=c3d95b014f453457d789e6cf61508351
+
+}
